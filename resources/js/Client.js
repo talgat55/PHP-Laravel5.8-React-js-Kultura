8647c4eb9144e03a792952797ccd5d72edde4c72
talgat55 <lightxdesign55@gmail.com>
2020-03-28 10:09:08 +0600

    redy preset react

---
+++ resources/js/Client.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import {render} from 'react-dom';
+
+export default class Client extends Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-md-8">
+                        <div className="card">
+                            <div className="card-header">Example Component</div>
+                            adwdawd
+                            <div className="card-body">I'm an example component!</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+    render(<Client/>, document.getElementById('root'));
+
