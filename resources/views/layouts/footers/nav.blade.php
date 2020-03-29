87b3331f5e6367bd1368daf722a247c3ac581de8
talgat55 <lightxdesign55@gmail.com>
2020-03-29 11:39:23 +0600

    delete copyright

--- resources/views/layouts/footers/nav.blade.php
+++ resources/views/layouts/footers/nav.blade.php
@@ -1,8 +1,8 @@
 <div class="row align-items-center justify-content-xl-between">
     <div class="col-xl-6">
-        <div class="copyright text-center text-xl-left text-muted">
-            <a target="_blank" class="bottom-copyright" title="Перейти на сайт разработчика" href="http://asmart-group.ru/">Разработан IT-company <span>ASMART</span></a>
-        </div>
+        {{--<div class="copyright text-center text-xl-left text-muted">--}}
+            {{--<a target="_blank" class="bottom-copyright" title="Перейти на сайт разработчика" href="http://asmart-group.ru/">Разработан IT-company <span>ASMART</span></a>--}}
+        {{--</div>--}}
     </div>
     <div class="col-xl-6">
 
